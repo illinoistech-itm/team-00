e1cfc13732dda05f403bf34c36fc9bd10cff2eea
Jeremy R Hajek <hajek@iit.edu>
2022-02-27 22:56:18 -0600

    update su command to switch to vagrant

--- build/packer-build-templates/scripts/post_install_ubuntu_lb.sh
+++ build/packer-build-templates/scripts/post_install_ubuntu_lb.sh
@@ -8,7 +8,9 @@ set -v
 
 sudo apt-get install -y nginx firewalld
 
-git clone git@github.com:illinoistech-itm/team-00.git
+#git clone git@github.com:illinoistech-itm/team-00.git
+
+su - vagrant -c "git clone git@github.com:illinoistech-itm/team-00.git"
 
 # Enable http in the firewall
 sudo firewall-cmd --zone=public --add-service=http --permanent
