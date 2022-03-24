bb497d7ca689683a316860f799de35e3243f09af
Jeremy R Hajek <hajek@iit.edu>
2022-03-23 21:42:14 -0500

    update vagrantfile and MYSQL

--- build/packer-build-templates/scripts/post_install_ubuntu_db.sh
+++ build/packer-build-templates/scripts/post_install_ubuntu_db.sh
@@ -30,7 +30,7 @@ sudo hostnamectl set-hostname db
 # the git clone command
 ##################################################################################
 su - vagrant -c "git clone git@github.com:illinoistech-itm/team-00.git"
-
+cd team-00/code/
 #################################################################################
 # Linux systemd Firewall - firewalld https://firewalld.org/
 # Remember to open proper firewall ports
@@ -42,7 +42,15 @@ sudo firewall-cmd --zone=public --add-source=192.168.56.0/24 --permanent
 # Reload changes to firewall
 sudo firewall-cmd --reload
 
-echo $USERPASS >> /home/vagrant/uservar.txt
+# Changing the mysql bind address with a script to listen for external connections
+# https://serverfault.com/questions/584607/changing-the-mysql-bind-address-within-a-script
+# https://stackoverflow.com/questions/23670282/bind-address-missing-in-my-cnf-in-mysql-centos
+# https://en.wikipedia.org/wiki/Sed
+# https://devopscube.com/setup-mysql-master-slave-replication/
+# Don't leave off the final "/" in a sed expression--they need to be closed
+# If using mysql instead of MariaDB the path to the cnf file is /etc/mysql/mysql.conf.d/mysql.cnf
+# sudo sed -i "s/.*bind-address.*/#bind-address = $(hostname -I | awk '{print $2}')/" /etc/mysql/mysql.conf.d/mysql.cnf
+sudo sed -i "s/.*bind-address.*/bind-address = $(hostname -I | awk '{print $2}')/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 #################################################################################
 # To execute .sql files to create tables, databases, and insert records
@@ -52,6 +60,14 @@ echo $USERPASS >> /home/vagrant/uservar.txt
 # These next 4 lines will create a simple database, a table, a non-root user
 # with limited permissions (adjust accordingly) and finally insert 3 dummy records
 #################################################################################
+
+#################################################################################
+# Using sed to replace placeholder variables in the scripts in the scripts 
+# with the USER variables passed from PACKER
+#################################################################################
+sed -i "s/\$ACCESSFROMIP/$ACCESSFROMIP/g" ./db-samples/*.sql
+sed -i "s/\$USERPASS/$USERPASS/g" ./db-samples/*.sql
+sed -i "s/\$USERNAME/$USENAME/g" ./db-samples/*.sql
 
 sudo mysql < /home/vagrant/team-00/code/db-samples/create-database.sql
 sudo mysql < /home/vagrant/team-00/code/db-samples/create-table.sql
