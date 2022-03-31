14aeafbba630007588541c774866a7965c318aee
Jeremy R Hajek <hajek@iit.edu>
2022-03-30 21:20:35 -0500

    added m1-remove-and-add

--- build/bash/m1-remove-and-add-vagrant-boxes.sh
+++ build/bash/m1-remove-and-add-vagrant-boxes.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 ######################################################################################
-# This PowerShell script will retrieve your team's Vagrant Boxes from the build-server
+# This Bash script will retrieve your team's Vagrant Boxes from the build-server
 # http://192.168.172.44/boxes
 # As each team is numbered this will allow you to retrieve and to issue the 
 # vagrant box add command at the same time.
@@ -7,10 +9,6 @@
 
 # Change XX to your team number with leading Zero
 # Create an array of system names
-if [ $# -eq 1 ]
-then
-num=$1
-echo "The team number you entered is $num"
 # $boxes="team$num-lb","team$num-ws1","team$num-ws2","team$num-ws3","team$num-db"
 DIRECTORIES=( lb ws1 ws2 ws3 db )
 ######################################################################################
@@ -30,7 +28,7 @@ do
   vagrant halt -f
   # Issuing the vagrant box destroy command to remove any delta files
   echo "Destroying vagrant box: $DIRECTORY"
-  vagrant destroy -f $DIRECTORY
+  vagrant destroy -f
   # Removing the previously registered vagrant boxes from the system
   echo "Removing vagrant box $DIRECTORY"
   vagrant box remove -f $DIRECTORY
@@ -43,21 +41,21 @@ do
 done
  
 ######################################################################################
-# Logic to retrieve the vagrant *.box files for your application from the build-server
-# bring them to your local system and issue the vagrant box add command
+# Logic to cd to the local build directory on your M1 mac -- where the Parallel Boxes
+# are located
 ######################################################################################
+cd ../packer-build-templates/build
+
+
 for DIRECTORY in ${DIRECTORIES[@]}
 do
     # Running the command to add the vagrant boxes, you can put a URL and Vagrant 
     # will retrieve the box for you in addition to adding the box
-    echo "Vagrant is retrieving and adding the box: team$num-$DIRECTORY-arm.box"
-    vagrant box add http://192.168.172.44/boxes/team$num-$DIRECTORY-arm.box --name $DIRECTORY
+    echo "Vagrant is adding the box: $DIRECTROY-arm.box"
+    vagrant box add $DIRECTROY-arm.box --name $DIRECTORY
 done
 # Show all the Vagrant boxes added properly
 vagrant box list 
 echo "All finished!"
 # Return to bash directory
-cd ../bash
-else
-  echo "To run the script you need to type: ./remove-and-retrieve-and-add-vagrant-boxes.sh XX -- where XX is your team number, with leading zero"
-fi  
+cd ../../bash
