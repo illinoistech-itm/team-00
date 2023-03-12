b7227aa539f64f82bed93a9caa4a9a9b9cd8a4df
jeremy hajek <hajek@iit.edu>
2023-03-12 17:15:57 -0500

    update adding database example

--- code/express-static-app/server.js
+++ code/express-static-app/server.js
@@ -1,5 +1,9 @@
 // server.js
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config()
+console.log(process.env)
+
 // Define Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +21,46 @@ app.get('/questions', (req,res) => {
 app.get('/user', (req,res) => {
     res.sendFile('user.html', { root: 'public' });
 });
+
+app.get('/db', (req,res) => {
+
+    var query_results = "";
+  
+  // create the connection to database
+   const connection = mysql.createConnection({
+     host: process.env.FQDN,
+       user: process.env.USER,
+       password: process.env.PASS,
+       database: process.env.HOST
+       });
+  
+  connection.connect((err) => {
+     if(err){
+        console.log('Error connecting to Db');
+         return;
+      }
+    console.log('Connection established');
+  });
+  
+  
+  // simple query
+   connection.query(
+     'SELECT * FROM `comment`;',
+       function(err, results, fields) {
+           query_results = results;
+           console.log(results); // results contains rows returned by server
+          // console.log(fields); // fields contains extra meta data about results, if available
+           console.log(err); // return the error
+       }
+              );
+  
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.write('The SQL query results are: ' + JSON.stringify(query_results));
+      res.write('Connection to MySQL status:' + conn_status)
+      res.end('Hello World' + host.hostname());
+  
+  });
 
 // Samples @ http://expressjs.com/en/starter/examples.html
 app.listen(PORT, () => {
